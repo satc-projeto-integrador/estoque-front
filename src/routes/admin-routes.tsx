fb319d506db555297f75f093deb7b93835c956c1
Vitor Henrique <vitorherpereira@gmail.com>
2023-09-05 21:01:23 -0300

    fix menu

--- src/routes/admin-routes.tsx
+++ src/routes/admin-routes.tsx
@@ -1,19 +1,17 @@
 import { Route } from "react-router-dom";
 import AdminLayout from "../layouts/admin-layout/admin-layout";
 import ListaUsuarios from "../features/usuarios/components/cadastro-usuarios/lista-usuarios";
-import Calendario from "../features/usuarios/components/calendar/calendario";
+/*import Calendario from "../features/usuarios/components/calendar/calendario";*/
 import { CalendarOutlined, HeartOutlined, HomeOutlined, LogoutOutlined, UserOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import NotFoundRedirect from "./not-found-redirect";
 import { Logout } from "../features/auth/logout";
-import ListaProfissionais from "../features/profissionais/components/lista-profissionais/lista-profissionais";
+/*import ListaProfissionais from "../features/profissionais/components/lista-profissionais/lista-profissionais";*/
 import ListaProdutos from "../features/produtos/components/lista-produtos/lista-produtos";
 
 const menuItens = [
     { key: 'home', label: 'Home', icon: <HomeOutlined /> },
     { key: 'usuarios', label: 'Usuários', icon: <UserOutlined /> },
-    { key: 'profissionais', label: 'Profissionais', icon: <HeartOutlined /> },
     { key: 'produtos', label: 'produtos', icon: <ShoppingCartOutlined /> },
-    { key: 'calendario', label: 'Calendário', icon: <CalendarOutlined /> },
     { key: 'logout', label: 'logout', icon: <LogoutOutlined /> },
 ]
 
@@ -24,9 +22,7 @@ export default function AdminRoutes() {
             <Route path='usuarios' element={<ListaUsuarios />} />
             {/* <Route path='cadastro' element={<CadastroUsuario />} /> */}
             {/* <Route path='consulta' element={<NovaConsulta />} /> */}
-            <Route path='profissionais' element={<ListaProfissionais />} />
             <Route path='produtos' element={<ListaProdutos />} />
-            <Route path='calendario' element={<Calendario />} />
             <Route path='logout' element={<Logout />} />
             <Route path="*" element={<NotFoundRedirect to="/home" />} />
         </Route>
